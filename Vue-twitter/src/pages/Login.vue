adc1d35653228885b9e506d27e6ed8e6f1885c33
YoungJinGo <skyyj32@naver.com>
2022-02-25 00:26:02 +0900

    ✨ 로그인, 회원가입 페이지 연결

--- Vue-twitter/src/pages/Login.vue
+++ Vue-twitter/src/pages/Login.vue
@@ -1,9 +1,70 @@
 <template>
-  <h1>Login Page</h1>
+  <div class="flex flex-col items-center space-y-4 mt-10">
+    <i
+      :class="`fab fa-twitter text-4xl text-primary  
+      ${loading ? 'animate-bounce' : ''}`"
+    ></i>
+    <span class="text-2xl font-bold">뜨위떠 로그인</span>
+    <input
+      type="text"
+      v-model="username"
+      class="
+        w-96
+        px-4
+        py-3
+        border border-gray-300
+        rounded
+        focus:ring-2 focus:border-primary focus:outline-none
+      "
+      placeholder="이메일"
+    />
+
+    <input
+      type="text"
+      v-model="password"
+      class="
+        rounded
+        w-96
+        px-4
+        py-3
+        border border-gray-300
+        focus:ring-2 focus:border-primary focus:outline-none
+      "
+      placeholder="비밀번호"
+    />
+    <button
+      @click="onLogin"
+      class="rounded-full w-96 bg-primary text-white py-3 hover:bg-dark"
+    >
+      회원가입
+    </button>
+    <router-link to="/register">
+      <button class="text-primary">계정이 없으신가요? 회원가입 하기</button>
+    </router-link>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const username = ref("");
+    const email = ref("");
+    const password = ref("");
+    const loading = ref(true);
+
+    const onLogin = () => {
+      console.log(username.value, email.value, password.value);
+    };
+    return {
+      username,
+      email,
+      password,
+      onLogin,
+      loading,
+    };
+  },
+};
 </script>
 
 <style>
--- Vue-twitter/src/pages/Register.vue
+++ Vue-twitter/src/pages/Register.vue
@@ -1,9 +1,82 @@
 <template>
-  <h1>Register page</h1>
+  <div class="flex flex-col items-center space-y-4 mt-10">
+    <i
+      :class="`fab fa-twitter text-4xl text-primary  
+      ${loading ? 'animate-bounce' : ''}`"
+    ></i>
+    <span class="text-2xl font-bold">뜨위떠 회원가입</span>
+    <input
+      type="text"
+      v-model="username"
+      class="
+        w-96
+        px-4
+        py-3
+        border border-gray-300
+        rounded
+        focus:ring-2 focus:border-primary focus:outline-none
+      "
+      placeholder="이메일"
+    />
+    <input
+      type="text"
+      v-model="email"
+      class="
+        rounded
+        w-96
+        px-4
+        py-3
+        border border-gray-300
+        focus:ring-2 focus:border-primary focus:outline-none
+      "
+      placeholder="아이디"
+    />
+    <input
+      type="text"
+      v-model="password"
+      class="
+        rounded
+        w-96
+        px-4
+        py-3
+        border border-gray-300
+        focus:ring-2 focus:border-primary focus:outline-none
+      "
+      placeholder="비밀번호"
+    />
+    <button
+      @click="onRegister"
+      class="rounded-full w-96 bg-primary text-white py-3 hover:bg-dark"
+    >
+      회원가입
+    </button>
+    <router-link to="/login">
+      <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
+    </router-link>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const username = ref("");
+    const email = ref("");
+    const password = ref("");
+    const loading = ref(true);
+
+    const onRegister = () => {
+      console.log(username.value, email.value, password.value);
+    };
+    return {
+      username,
+      email,
+      password,
+      onRegister,
+      loading,
+    };
+  },
+};
 </script>
 
 <style>
