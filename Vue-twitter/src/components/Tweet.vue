3c20f47b340a2cd90a7384c6f54d1e858c170656
YoungJinGo <skyyj32@naver.com>
2022-03-28 20:02:48 +0900

    ✨ Tweet static data -> userdata Setting

--- Vue-twitter/src/components/Tweet.vue
+++ Vue-twitter/src/components/Tweet.vue
@@ -16,33 +16,32 @@
     />
     <div class="flex-1 flex flex-col ml-3 space-y-2">
       <div class="text-sm space-x-1">
-        <span class="font-bold">조국</span>
-        <span class="text-gray-500 text-xs">@skyyj32</span>
+        <span class="font-bold">{{ tweet.uid }}</span>
+        <span class="text-gray-500 text-xs">@</span>
         <span>.</span>
-        <span class="text-gray-500 text-xs">7분</span>
+        <span class="text-gray-500 text-xs">{{
+          moment(tweet.create_at).fromNow()
+        }}</span>
       </div>
       <!-- tweet body -->
       <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        voluptatem molestiae amet quae iure, impedit eius explicabo quas, modi
-        itaque voluptas eaque illum nesciunt blanditiis ad tempore quasi!
-        Molestiae, nisi?
+        {{ tweet.tweet_body }}
       </div>
       <!-- tweet actions -->
       <div class="flex justify-between pr-12">
         <div class="text-gray-500 hover:text-primary">
           <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
-          <span class="ml-1 text-sm p-1">11</span>
+          <span class="ml-1 text-sm p-1">{{ tweet.num_comments }}</span>
         </div>
 
         <div class="text-gray-500 hover:text-green-500">
           <i class="fa fa-retweet hover:bg-green-50 p-2 rounded-full"></i>
-          <span class="ml-1 text-sm p-1">11</span>
+          <span class="ml-1 text-sm p-1">{{ tweet.num_retweets }}</span>
         </div>
 
         <div class="text-gray-500 hover:text-red-500">
           <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
-          <span class="ml-1 text-sm p-1">11</span>
+          <span class="ml-1 text-sm p-1">{{ tweet.num_likes }}</span>
         </div>
 
         <div class="text-gray-500 hover:text-primary rounded-full">
@@ -62,8 +61,15 @@
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
-  props: ["currentUser"],
+  props: ["currentUser", "tweet"],
+  setup() {
+    return {
+      moment,
+    };
+  },
 };
 </script>
 
